d3f9b886f5d533a23f04a692a108522439932760
Accuvend <149505926+Accuvend@users.noreply.github.com>
2024-06-20 15:45:13 +0100

    Update build.sh
    - Updated Cache

--- build.sh
+++ build.sh
@@ -4,4 +4,10 @@ cp package.json dist/package.json
 ls ./src/utils/Email/templates && cp -r src/utils/Email/templates dist/utils/Email/  && rm -f ./dist/utils/Email/templates/index.ts  && ls ./dist/utils/Email/templates
 cp newrelic.js ./dist/newrelic.js
 rm -rf dist.zip
+if [-f "./.env"]; then 
+  cp .env ./dist/.env
+  echo ".env file found and copied"
+else
+  echo ".env file does not exist"
+fi 
 zip -r dist.zip dist
